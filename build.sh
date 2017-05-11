729bccd0b08c9a069c61cc6f93f4a8305fe479a5
Joshua Homer <countjoshington@gmail.com>
2017-05-10 21:46:14 -0600

    updated dotnet test call

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 dotnet restore SofiAssignment.sln
 dotnet build SofiAssignment.sln -f net46
-dotnet test SofiAssignment.sln -f net46
+dotnet test SofiAssignment.Tests/SofiAssignment.Tests.csproj -f net46
